a4df6c77d2a0243f45766ab9521410a2b55846a2
JL <mosqueramendozajl@gmail.com>
2025-06-26 21:34:37 -0500

    Initial commit - Angular financial management application

---
+++ src/app/components/bono/listarbono/listarbono.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Bono } from '../../../models/bono';
+import { BonoService } from '../../../services/bono.service';
+import {MatIconModule} from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-listarbono',
+  imports: [MatTableModule, MatIconModule, RouterModule],
+  templateUrl: './listarbono.component.html',
+  styleUrl: './listarbono.component.css'
+})
+export class ListarbonoComponent implements OnInit {
+  dataSource: MatTableDataSource<Bono> = new MatTableDataSource();
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6','c7'];
+
+
+  constructor(private bS: BonoService){}
+  ngOnInit(): void {
+    this.bS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data)
+    });
+    this.bS.getlist().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+  
+}
