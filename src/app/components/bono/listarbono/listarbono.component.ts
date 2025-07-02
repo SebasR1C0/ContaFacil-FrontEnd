9869bcb505ee53bba93982fc289db5b63228520b
JL <mosqueramendozajl@gmail.com>
2025-07-02 18:09:12 -0500

    Eliminación del boton manual en el encabezado del app component y reorganizacion de acciones en listar bono en una sola columna

--- src/app/components/bono/listarbono/listarbono.component.ts
+++ src/app/components/bono/listarbono/listarbono.component.ts
@@ -17,7 +17,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class ListarbonoComponent implements OnInit {
   dataSource: MatTableDataSource<Bono> = new MatTableDataSource();
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6','c7', 'c8', 'c9'];
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'];
 
 
   constructor(
